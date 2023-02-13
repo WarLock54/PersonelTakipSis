d007f563c81669feef27a2ad98578acb29fabc45
pRide01 <onr_540@hotmail.com>
2023-02-13 14:04:17 +0300

    Ekleme
    SOAP protokolu eklendi

---
+++ ANA PROJE/CarPark.User/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using CarPark.Core.Repository.Abstract;
+using CarPark.Entities.concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarPark.User.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IRepository<Personel> _personelRepository;
+        public UserController(IRepository<Personel> personelRepository)
+        {
+            _personelRepository = personelRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+
+            
+
+            return View();
+        }
+    }
+}
